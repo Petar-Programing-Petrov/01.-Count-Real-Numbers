90bd4ddf924e159e3509d3155e8700c45ac97e6e
Petar Petrov <petar.dragov.petrov@gmail.com>
2020-12-14 09:45:45 +0100

    Add project files.

---
+++ Associative Arrays - Lab/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Associative_Arrays___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts.Add(number, 1);
+                }
+
+            }
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
